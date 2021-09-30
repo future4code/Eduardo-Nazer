b38ad881eef7928914041cdc5cf604aba3e6ca3a
EduNazer <nazer.ws@gmail.com>
2021-09-30 14:02:11 -0300

    tentando fazer um PR, estou tendo algumas falhas

--- quinzena2/loops/index.js
+++ quinzena2/loops/index.js
@@ -31,29 +31,35 @@
     // O console irá imprimir os números 19, 21, 23, 25, 27, 30.
 
 //         b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se não, o que poderia ser usado para fazer isso?
+     
+            // const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+            // for (nano of lista){
+            //     if (nano > 0)
+            //     nano++
+            //     console.log(nano)
+            // }
 
-    //Não é o suficiente, teria que utilizar o comando dessa forma: 
-    
-        const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-        for (let numero of lista) {
-          if (numero > 18) {
-        		console.log(numero)
-        	}
-        }
-    
+
+            
+            
+            //incompleto
+
+
+
+
 //     3. Qual seria o resultado impresso no console, se o usuário digitasse o número `4` ?
         
 //         ```jsx
-//         const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-//         let quantidadeAtual = 0
-//         while(quantidadeAtual < quantidadeTotal){
-//           let linha = ""
-//           for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-//             linha += "*"
-//           }
-//           console.log(linha)
-//           quantidadeAtual++
-//         }
+        const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+        let quantidadeAtual = 0
+        while(quantidadeAtual < quantidadeTotal){
+          let linha = ""
+          for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+            linha += "*"
+          }
+          console.log(linha)
+          quantidadeAtual++
+        }
         
 //         ```
         
